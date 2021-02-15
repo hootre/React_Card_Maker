7646ec7158fdff911956cbf233b965e91981722a
hootre <zxv7295@naver.com>
2021-02-15 15:40:10 +0900

    React-Redux Study-3

---
+++ src/containers/todosContainer.js
@@ -0,0 +1,18 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Todos from "../components/todos/todos";
+import { addTodo, toggleTodo } from "../modules/todos";
+
+const TodosContainer = () => {
+  //  useSelector 에서 꼭 객체를 반환 할 필요는 없습니다.
+  //  한 종류의 값만 조회하고 싶으면 그냥 원하는 값만 바로 반환하면 된다.
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const onCreate = useCallback((text) => dispatch(addTodo(text)), [dispatch]);
+  const onToggle = useCallback((id) => dispatch(toggleTodo(id)), [dispatch]);
+
+  return <Todos todos={todos} onCreate={onCreate} onToggle={onToggle} />;
+};
+
+export default TodosContainer;
